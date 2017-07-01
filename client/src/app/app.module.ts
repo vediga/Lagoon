d80f4ea0a60d1ad51883606f769b62cb3d59c0a3
Vikas Ediga <vediga@vmware.com>
2017-07-01 13:02:54 -0700

    [client] Added nav bar component
    - some formatting

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home.component';
 import { PhotoListComponent } from './features/photo-list/photo-list.component';
 import { SidePanelComponent } from './features/side-panel/side-panel.component';
+import { NavBarComponent } from './features/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PhotoListComponent,
-    SidePanelComponent
+    SidePanelComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
